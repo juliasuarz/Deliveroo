e32328fccb2a940f98a98f3dd29e003a116e86e4
Carla Maldonado <113420553+carlamaldonado04@users.noreply.github.com>
2024-02-22 16:02:51 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 document.addEventListener("DOMContentLoaded", function() {
-    // Obtener el elemento con el id "miDesplegable"
-    var desplegable = document.getElementById("desplegable");
-  
-    // Agregar un evento de clic al elemento
-    desplegable.addEventListener("click", function() {
-      // Toggle de la clase "desplegado"
-      this.classList.toggle("desplegado");
-    });
+  // Obtener los elementos con los IDs "desplegable" y "desplegable1"
+  var desplegable1 = document.getElementById("desplegable1");
+  var desplegable2 = document.getElementById("desplegable2");
+
+  // Agregar un evento de clic a cada elemento
+  desplegable1.addEventListener("click", function() {
+    // Toggle de la clase "in"
+    this.classList.toggle("in");
   });
+
+  desplegable2.addEventListener("click", function() {
+    // Toggle de la clase "in"
+    this.classList.toggle("in");
+  });
+});
+
+$('.carousel[data-type="multi"] .item').each(function(){
+  var next = $(this).next();
+  if (!next.length) {
+    next = $(this).siblings(':first');
+  }
+  next.children(':first-child').clone().appendTo($(this));
+
+  for (var i=0;i<4;i++) {
+    next=next.next();
+    if (!next.length) {
+    	next = $(this).siblings(':first');
+  	}
+    
+    next.children(':first-child').clone().appendTo($(this));
+  }
+});
+
