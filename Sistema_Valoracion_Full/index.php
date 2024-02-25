2d6a4c5d489a9de84a605c9077dbd32964a1ac14
danielfont-z <danfoncap@gmail.com>
2024-02-25 18:37:17 +0100

    commit 25/02/24
    Sistema de valoración acabado.
    Tendré que modificarlo ahora con colores y diseño que pegue con Deliveroo

--- Sistema_Valoracion_Full/index.php
+++ Sistema_Valoracion_Full/index.php
@@ -28,7 +28,7 @@
                             <i class="fas fa-star star-light mr-1 main_star"></i>
                             <i class="fas fa-star star-light mr-1 main_star"></i>
 	    				</div>
-    					<h3><span id="total_review">0</span> Reseña</h3>
+    					<h3><span id="total_review">0</span> Reseña/s</h3>
     				</div>
     				<div class="col-sm-4">
     					<p>
@@ -149,8 +149,7 @@
 
             reset_background();
 
-            for(var count = 1; count <= rating; count++)
-            {
+            for(var count = 1; count <= rating; count++){
 
                 $('#submit_star_'+count).addClass('text-warning');
 
@@ -158,10 +157,8 @@
 
         });
 
-        function reset_background()
-        {
-            for(var count = 1; count <= 5; count++)
-            {
+        function reset_background(){
+            for(var count = 1; count <= 5; count++){
 
                 $('#submit_star_'+count).addClass('star-light');
 
@@ -174,8 +171,7 @@
 
             reset_background();
 
-            for(var count = 1; count <= rating_data; count++)
-            {
+            for(var count = 1; count <= rating_data; count++){
 
                 $('#submit_star_'+count).removeClass('star-light');
 
@@ -196,28 +192,93 @@
 
             var user_review = $('#user_review').val();
 
-            if(user_name == '' || user_review == '')
-            {
+            if(user_name == '' || user_review == ''){
                 alert("Rellena ambos campos");
                 return false;
-            }
-            else
-            {
+            } else{
                 $.ajax({
                     url:"submit_rating.php",
                     method:"POST",
                     data:{rating_data:rating_data, user_name:user_name, user_review:user_review},
-                    success:function(data)
-                    {
+                    success:function(data){
                         $('#review_modal').modal('hide');
-
-                        // load_rating_data();
-
+                        load_rating_data();
                         alert(data);
                     }
                 })
             }
 
         });
+
+        load_rating_data();
+
+        function load_rating_data(){
+            $.ajax({
+                url:"submit_rating.php",
+                method:"POST",
+                data:{action:'load_data'},
+                dataType:"JSON",
+                success:function(data){
+                    $('#average_rating').text(data.average_rating);
+                    $('#total_review').text(data.total_review);
+
+                    var count_star = 0;
+
+                    $('.main_star').each(function(){
+                        count_star++;
+                        if(Math.ceil(data.average_rating) >= count_star){
+                            $(this).addClass('text-warning');
+                            $(this).addClass('star-light');
+                        }
+                    });
+
+                    $('#total_five_star_review').text(data.five_star_review);
+                    $('#total_four_star_review').text(data.four_star_review);
+                    $('#total_three_star_review').text(data.three_star_review);
+                    $('#total_two_star_review').text(data.two_star_review);
+                    $('#total_one_star_review').text(data.one_star_review);
+
+                    $('#five_star_progress').css('width', (data.five_star_review/data.total_review) * 100 + '%');
+                    $('#four_star_progress').css('width', (data.four_star_review/data.total_review) * 100 + '%');
+                    $('#three_star_progress').css('width', (data.three_star_review/data.total_review) * 100 + '%');
+                    $('#two_star_progress').css('width', (data.two_star_review/data.total_review) * 100 + '%');
+                    $('#one_star_progress').css('width', (data.one_star_review/data.total_review) * 100 + '%');
+
+                    if(data.review_data.length > 0){
+                        var html = '';
+
+                        for(var count = 0; count < data.review_data.length; count++){
+                            html += '<div class="row mb-3">';
+                            html += '<div class ="col-sm-1"><div class="rounded-circle bg-danger text-white pt-2 pb-2"><h3 class="text-center">'+data.review_data[count].user_name.charAt(0)+'</h3></div></div>';
+                            html += '<div class="col-sm-11">';
+                            html += '<div class="card">';
+                            html += '<div class="card-header"><b>'+data.review_data[count].user_name+'</b></div>';
+                            html += '<div class="card-body">';
+                            for(var star = 1; star <= 5; star++){
+                                var class_name = '';
+
+                                if(data.review_data[count].rating >= star){
+                                    class_name = 'text-warning';
+                                } else {
+                                    class_name = 'star-light';
+                                }
+
+                                html += '<i class="fas fa-star '+class_name+' mr-1"></i>';
+                            }
+                            html += '<br />';
+                            html += data.review_data[count].user_review;
+                            html += '</div>';
+                            html += '<div class="card-footer text-right">Publicado el '+data.review_data[count].datetime+'</div>';
+                            html += '</div>';
+                            html += '</div>';
+                            html += '</div>';
+                        }
+
+                        $('#review_content').html(html);
+                    }
+                }
+            })
+        }
+
     });
 </script>
