0c273a3a41ea664567bda2a080ef7609cc83c83d
danielfont-z <danfoncap@gmail.com>
2024-02-26 21:50:19 +0100

    sweetalerts

--- Sistema_Valoracion_Full/index.php
+++ Sistema_Valoracion_Full/index.php
@@ -8,7 +8,8 @@
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container">
@@ -210,7 +211,11 @@
             var user_review = $('#user_review').val();
 
             if(user_name == '' || user_review == ''){
-                alert("Rellena ambos campos");
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Error',
+                    text: 'Rellena ambos campos'
+                });
                 return false;
             } else{
                 $.ajax({
@@ -220,7 +225,11 @@
                     success:function(data){
                         $('#review_modal').modal('hide');
                         load_rating_data();
-                        alert(data);
+                        Swal.fire({
+                            icon: 'success',
+                            title: '¡Perfecto!',
+                            text: data
+                        });
                     }
                 })
             }
